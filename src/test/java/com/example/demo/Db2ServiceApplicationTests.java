476f5efb987ad83fbd84438e4f1fd46e827f5ce5
n0214816 <n0214816@VDDP35X-KQ3FMJ8.lm.lmig.com>
2018-07-21 02:01:23 -0400

    added test case

--- src/test/java/com/example/demo/Db2ServiceApplicationTests.java
+++ src/test/java/com/example/demo/Db2ServiceApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import junit.framework.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Db2ServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void chumma() {
+		Assert.fail();
 	}
 
 }
